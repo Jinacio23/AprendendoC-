967775277a926cce13d1a7d8aced20738693da75
Jinacio23 <jinaciocemar@gmail.com>
2025-03-27 11:28:29 -0300

    idade

---
+++ Idade/Program.cs
@@ -0,0 +1,22 @@
+﻿
+class Program
+{
+    public static void Main()
+    {
+
+
+        Console.WriteLine("Informe seu ano de nascimento: ");
+
+        int anoNasc = Convert.ToInt32(Console.ReadLine());
+
+
+        Console.WriteLine("Informe o ano atual: ");
+
+        int anoAtual = Convert.ToInt32(Console.ReadLine());
+
+        int idade = anoAtual - anoNasc;
+
+        Console.WriteLine($"Sua idade ao fim do ano será: {idade}");
+
+    }
+}
