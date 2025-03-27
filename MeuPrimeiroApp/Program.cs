920bacab22ac9a43ccf15f40978a0d6dc3d20dc5
Jinacio23 <jinaciocemar@gmail.com>
2025-03-27 10:56:50 -0300

    Tipos

---
+++ MeuPrimeiroApp/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        //Tipo nomeDaVariavel
+
+        double num1, num2;
+
+        Console.WriteLine("Informe um número:");
+        num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Informe outro número:");
+        num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine(" ");
+
+        double soma =(double) num1 + (double)num2;
+        double subt = (double) num1 - (double)num2;
+        double div = num1 / num2;
+        double multi = (double) num1 * (double)num2;
+
+        Console.WriteLine($"A soma dos valores é: \n {soma} \n");
+        Console.WriteLine($"A subtração dos valores é: \n {subt} \n");
+        Console.WriteLine($"A divisão dos valores é: \n {div} \n");
+        Console.WriteLine($"A multiplicação dos valores é: \n {multi} \n");
+
+
+        Console.WriteLine("\n Digite um nome");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine("{0} fez a soma de {1} + {2} e deu {3}", nome, num1, num2, soma);
+    }
+}
---
+++ Tipos/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        //Tipos numéricos 
+        int numeroInt  = 32;
+
+        //Por padrão valores quebrados são definidos como 'double'
+        //float - numeros com pontos fllutuantes - menor precisão
+        //(decimais)
+
+        float numeroFloat = (float)11.5f;
+
+        //double - numeros com pontos fllutuantes - maior precisão
+        //(decimais)
+        double numeroDouble = 5.190890890890890889;
+        double numeroDouble2 = 50;
+
+        //double - 123.49586
+        decimal numeroDecimal = (decimal)10928390.91208239048903223;
+
+        //Tipos textuais
+
+        //char - apenas um caracter
+        //uso de aspas simples
+        char caracter = 'A';        
+        char caracter2 = (char)85;
+        char caracter3 = '5';
+
+
+        //string - grupo de caracteres
+        // uso de aspas duplas
+
+        string texto = "adlksjdfkdsjfksd xfsdfs sdf";
+        string texto2 = "A";
+    }
+}
